cfff9ae54ca34a867ecfe02c744bf156416ce0a7
AniaLaz <lazarenko.ania.sg@gmail.com>
2022-10-08 16:22:18 +0300

    2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,5 +1,5 @@
-import flatpickr from "flatpickr";
-import "flatpickr/dist/flatpickr.min.css"
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
 
 const inputEl = document.querySelector('#datetime-picker');
 const butnStart = document.querySelector('[data-start]');
@@ -10,12 +10,7 @@ const dataSeconds = document.querySelector('[data-seconds]');
 
 const currentDate = new Date()
 const currentTime = currentDate.getTime()
-
-console.log('currentDate :', currentDate);
-// console.log("calendar :", options.defaultDate);
-
-inputEl.addEventListener('input', onCalendar)
-
+let calendarTime = currentDate.getTime()
 
 const options = {
     enableTime: true,
@@ -23,29 +18,150 @@ const options = {
     defaultDate: new Date(),
     minuteIncrement: 1,
     onClose(selectedDates) {
-      // console.log(selectedDates[0].getHours());
+      console.log(selectedDates[0])
+      if (currentTime <= calendarTime){
+        butnStart.disabled = false;
+      }
+      else {
+        butnStart.disabled = true;}
        },
   };
 
-  const dataPickr = new flatpickr(inputEl, options)
+const dataPickr = new flatpickr(inputEl, options)
+
+inputEl.addEventListener('input', onCalendar)
 
 butnStart.disabled = true;
 
 
 function onCalendar() {
-  console.log("hi");
-  // console.log("calendar :", options.defaultDate.getHours());
-  console.log("calendar Hours:", dataPickr.selectedDates.selectedDates[0].getHours())
-  console.log("calendar Minutes:", dataPickr.selectedDates.selectedDates[0].getMinutes())
-  console.log("calendar Seconds:", dataPickr.selectedDates.selectedDates[0].getSeconds())
+//   console.log("calendar Hours:", dataPickr.selectedDates[0].getHours())
+//   console.log("calendar Minutes:", dataPickr.selectedDates[0].getMinutes())
+//   console.log("calendar Seconds:", dataPickr.selectedDates[0].getSeconds())
 
+  calendarTime = dataPickr.selectedDates[0].getTime()
 
-  dataHours.textContent = dataPickr.selectedDates.selectedDates[0].getHours();
-  dataMinutes.textContent = dataPickr.selectedDates.selectedDates[0].getMinutes();
-  dataSeconds.textContent = dataPickr.selectedDates.selectedDates[0].getSeconds();
+  dataHours.textContent = dataPickr.selectedDates[0].getHours();
+  dataMinutes.textContent = dataPickr.selectedDates[0].getMinutes();
+  dataSeconds.textContent = dataPickr.selectedDates[0].getSeconds();
+  console.log('currentDate :', currentTime);
+  console.log("calendar :", calendarTime);
  }
 
 
+ 
 
 
 
+
+
+
+
+
+
+// const refs = {
+//   inputEl: document.querySelector('#datetime-picker'),
+//   startBtn: document.querySelector('button[data-start]'),
+//   daysCouner: document.querySelector('span[data-days]'),
+//   hoursCouner: document.querySelector('span[data-hours]'),
+//   minutesCouner: document.querySelector('span[data-minutes]'),
+//   secondsCouner: document.querySelector('span[data-seconds]'),
+// };
+// let timerId = null;
+// let isActiveTimer = false;
+// const INTERVAL = 1000;
+// refs.startBtn.classList.add('timer-btn');
+// const options = {
+//   enableTime: true,
+//   time_24hr: true,
+//   defaultDate: new Date(),
+//   minuteIncrement: 1,
+//   onChange(selectedDates) {
+//     if (selectedDates[0] <= Date.now()) {
+//       refs.startBtn.setAttribute('disabled', '');
+//     } else {
+//       if (refs.startBtn.hasAttribute('disabled')) {
+//         refs.startBtn.removeAttribute('disabled');
+//       }
+//     }
+//   },
+//   onClose(selectedDates) {
+//     if (selectedDates[0] <= Date.now()) {
+//       refs.startBtn.setAttribute('disabled', '');
+
+//       Report.failure(
+//         'Attention!',
+//         'Please choose a date in the future',
+//         'Okay',
+//         {
+//           width: '300px',
+//           titleFontSize: '22px',
+//           messageFontSize: '16px',
+//         }
+//       );
+//     }
+//   },
+// };
+
+// const fp = flatpickr(refs.inputEl, options);
+// refs.startBtn.addEventListener('click', onStartBtnClick);
+
+// function onStartBtnClick() {
+//   if (isActiveTimer) {
+//     return;
+//   }
+//   isActiveTimer = true;
+
+//   timerId = setInterval(() => {
+//     const currentTime = Date.now();
+//     const ms = fp.selectedDates[0] - currentTime;
+
+//     if (ms >= 0) {
+//       const getTime = convertMs(ms);
+
+//       updateTimerFace(getTime);
+//     } else {
+//       stopTimer(timerId);
+//       // window.alert('Game is over');
+//       Report.info('Too late...', 'Time is over', 'Okay', {
+//         width: '300px',
+//         titleFontSize: '22px',
+//         messageFontSize: '16px',
+//       });
+//     }
+//   }, INTERVAL);
+// }
+
+// function stopTimer(timerId) {
+//   clearInterval(timerId);
+//   isActiveTimer = false;
+// }
+// function updateTimerFace({ days, hours, minutes, seconds }) {
+//   refs.daysCouner.textContent = days;
+//   refs.hoursCouner.textContent = hours;
+//   refs.minutesCouner.textContent = minutes;
+//   refs.secondsCouner.textContent = seconds;
+// }
+// function convertMs(ms) {
+//   // Number of milliseconds per unit of time
+//   const second = 1000;
+//   const minute = second * 60;
+//   const hour = minute * 60;
+//   const day = hour * 24;
+
+//   // Remaining days
+//   const days = addLeadingZero(Math.floor(ms / day));
+//   // Remaining hours
+//   const hours = addLeadingZero(Math.floor((ms % day) / hour));
+//   // Remaining minutes
+//   const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+//   // Remaining seconds
+//   const seconds = addLeadingZero(
+//     Math.floor((((ms % day) % hour) % minute) / second)
+//   );
+
+//   return { days, hours, minutes, seconds };
+// }
+// function addLeadingZero(value) {
+//   return String(value).padStart(2, '0');
+// }
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,26 @@
+const inputDelay = document.querySelector('[name="delay"]')
+const inputStep = document.querySelector('[name="step"]')
+const inputAmount= document.querySelector('[name="amount"]')
+console.log(inputDelay.value);
+console.log(inputStep.value);
+console.log(inputAmount.value);
+
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
     // Fulfill
+    position
   } else {
     // Reject
   }
 }
+
+
+// createPromise(2, inputDelay.value)
+//   .then(({ position, delay }) => {
+//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//   })
+//   .catch(({ position, delay }) => {
+//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+//   });
