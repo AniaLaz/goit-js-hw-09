4b346982d26ea6e76bb90537915739ff5bd61165
AniaLaz <104758969+AniaLaz@users.noreply.github.com>
2022-10-08 13:22:39 +0000

    Deploying to gh-pages from @ AniaLaz/goit-js-hw-09@cfff9ae54ca34a867ecfe02c744bf156416ce0a7 🚀

---
+++ 03-promises.5cefa881.js
@@ -0,0 +1,2 @@
+const e=document.querySelector('[name="delay"]'),o=document.querySelector('[name="step"]'),l=document.querySelector('[name="amount"]');console.log(e.value),console.log(o.value),console.log(l.value);
+//# sourceMappingURL=03-promises.5cefa881.js.map
